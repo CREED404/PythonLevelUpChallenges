9d23b16eb74b174746c948654360c61c9626549e
Amin Siddiqui <staticVoidMan@users.noreply.github.com>
2023-05-13 14:03:03 +0530

    #02 - Regex shortcut

--- src/02 - Palindrome/main.py
+++ src/02 - Palindrome/main.py
@@ -1,26 +1,9 @@
 # Go hang a salami, I’m a lasagna hog.
 # gohangasalamiimalasgnahog is a palindrome
 
+import re
+
 def is_palindrome(string):
-  i = 0
-  j = len(string) - 1
-
-  while i < j:
-    lhs = string[i]
-    rhs = string[j]
-
-    if not lhs.isalpha():
-      i += 1
-      continue
-
-    if not rhs.isalpha():
-      j -= 1
-      continue
-
-    if lhs.lower() != rhs.lower():
-      return False
-
-    i += 1
-    j -= 1
-  
-  return True
+  lhs = ''.join(re.findall(r'[a-z]', string.lower()))
+  rhs = lhs[::-1]
+  return lhs == rhs
