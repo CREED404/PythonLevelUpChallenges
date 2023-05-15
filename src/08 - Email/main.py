e7ed25b41d483a846067d0c7c1952fb3a05d3899
Amin Siddiqui <staticVoidMan@users.noreply.github.com>
2023-05-15 18:36:42 +0530

    #08 - Improved with `email.mime` module

--- src/08 - Email/main.py
+++ src/08 - Email/main.py
@@ -1,9 +1,23 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 def send_email(sender, password, receiver, subject, body):
-  message = f"Subject:{subject}\n\n{body}"
+  email = MIMEMultipart()
+  email["From"] = sender
+  email["To"] = receiver
+  email["Subject"] = subject
 
-  with smtplib.SMTP("smtp.gmail.com", 587) as server:
-    server.starttls()
-    server.login(sender, password)
-    server.sendmail(sender, receiver, message)
+  email.attach(MIMEText(body, "plain"))
+
+  raw = email.as_string()
+  print(raw)
+
+  try:
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+      server.starttls()
+      server.login(sender, password)
+      server.sendmail(sender, receiver, raw)
+    print(f"Email sent successfully to {receiver}")
+  except smtplib.SMTPException as e:
+    print(e)
