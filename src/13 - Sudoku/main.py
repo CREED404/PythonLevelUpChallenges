61ca5a8ebee494107f33b61cc0bc64d0e3d00c75
Amin Siddiqui <staticVoidMan@users.noreply.github.com>
2023-05-17 16:02:49 +0530

    #13 - memory optimization using `deque` module

--- src/13 - Sudoku/main.py
+++ src/13 - Sudoku/main.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def print_sudoku(puzzle):
   for i, line in enumerate(puzzle):
     formatted = []
@@ -7,52 +9,39 @@ def print_sudoku(puzzle):
         formatted.append("|")
     print('  '.join(formatted))
     if i in [2,5]:
-      print('-------------------------------')
+      print("-" * 31)
 
 def solve_sudoku(puzzle):
   print("INPUT:")
   print_sudoku(puzzle)
 
-  # Find missing indices
-  missing_indices = []
-  for i, array in enumerate(puzzle):
-    for j, el in enumerate(array):
-      if el == 0:
-        missing = (i, j)
-        missing_indices.append(missing)
+  # Using double-ended queue for cheaper cost of rotate/pop operation of just 1 element compared
+  # to list when removing an element from the middle which would cost shifting subsequent elements
+  missing_indices = deque((i, j) for i, array in enumerate(puzzle) for j, el in enumerate(array) if el == 0)
 
-  options = set(range(1,10))
-
-  i = 0
-  while len(missing_indices) > 0:
-    (x, y) = missing_indices[i]
+  while missing_indices:
+    (x, y) = missing_indices[0]
     
-    known = set()
-    horizontal = puzzle[x]
-    vertical = [line[y] for line in puzzle]
+    horizontal = set(puzzle[x])
+    vertical = {line[y] for line in puzzle}
     
-    box = []
-    box_lower_v = (x // 3) * 3
-    box_higher_v = box_lower_v + 3
-    box_lower_h = (y // 3) * 3
-    box_higher_h = box_lower_h + 3
-    for box_y in range(box_lower_v, box_higher_v):
-      box += puzzle[box_y][box_lower_h:box_higher_h]
+    box_y1 = (x // 3) * 3
+    box_y2 = box_y1 + 3
+    box_x1 = (y // 3) * 3
+    box_x2 = box_x1 + 3
+    box = {puzzle[box_y][box_x] for box_y in range(box_y1, box_y2) for box_x in range(box_x1, box_x2)}
 
-    known.update(horizontal)
-    known.update(vertical)
-    known.update(box)
+    known = horizontal | vertical | box
     known.remove(0)
 
-    unknown = known.symmetric_difference(options)
+    unknown = set(range(1,10)) - known
 
     found = len(unknown) == 1
     if found:
       puzzle[x][y] = unknown.pop()
-      missing_indices.pop(i)
-      i = 0 if i >= len(missing_indices) else i
+      missing_indices.popleft()
     else:
-      i = (i + 1) % len(missing_indices)
+      missing_indices.rotate(-1)
   
   print("OUTPUT:")
   print_sudoku(puzzle)
