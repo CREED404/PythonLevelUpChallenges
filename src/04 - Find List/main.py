b402f40268074b74d4154cb84bc5e006e7e28ddf
CREED404 <116465479+CREED404@users.noreply.github.com>
2024-03-01 16:16:05 +0100

    Update main.py

--- src/04 - Find List/main.py
+++ src/04 - Find List/main.py
@@ -2,14 +2,30 @@
 # find 2         : [[0, 0, 1], [0, 1], [1, 1]]
 # find [1, 2, 3] : [[0, 0], [1]]
 
-def index_all(array, pattern):
-  result = []
 
-  for i, el in enumerate(array):
-    if el == pattern:
-      result.append([i])
-    elif isinstance(el, list):
-      for j in index_all(el, pattern):
-        result.append([i] + j)
-
-  return result
+def index_all(theList: list, value):
+    indexes = []
+    value   = value
+    level   = 0
+    tmp=[]
+    def recursion(listOrValue: list, i=None):
+        nonlocal indexes
+        nonlocal level
+        nonlocal tmp
+        if listOrValue == value:
+            #print(f"{index=}, {level=}, {listOrValue=}")
+            indexes.append(tmp.copy())
+            print("now: ", tmp)
+            return
+        if isinstance(listOrValue, list):
+            level+=1
+            if level != len(tmp):
+                tmp.append(0)
+            for i, _list in enumerate(listOrValue):
+                tmp[level-1]=i
+                print("tmp: ", tmp)
+                recursion(_list, i)
+            level-=1
+            tmp=tmp[0:-1]
+    recursion(theList)
+    return indexes
