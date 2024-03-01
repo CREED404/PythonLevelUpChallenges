6fb950af02469216df6b4a9d32945844cce223c9
CREED404 <116465479+CREED404@users.noreply.github.com>
2024-03-01 22:41:19 +0100

    Update main.py

--- src/09 - Random/main.py
+++ src/09 - Random/main.py
@@ -1,20 +1,25 @@
-from random import randint
-from collections import Counter
+from collections import defaultdict
 
-def roll_dice(*args):
-  attempts = 1_000_000
-  lowerbound = len(args)
-  upperbound = sum(args)
-  
-  results = Counter()
-  for _ in range(attempts):
-    roll = (randint(1, arg) for arg in args)
-    rollSum = sum(roll)
-    
-    results[rollSum] += 1
-  
-  print("OUTCOME PROBABILITY")
-  for i in range(lowerbound, upperbound + 1):
-    occurances = results[i]
-    probability = occurances / attempts * 100
-    print(f"{i}\t{probability: .2f}%")
+def roll_dice(*arg):
+    number_of_tests = 1_000_000
+    dices = []
+    for dice in arg:
+        tmp = []
+        for side in range(1, dice+1):
+            tmp.append(side)
+        dices.append(tmp)
+    #print(dices)
+    tests = []
+    for _ in range(0, number_of_tests+1):
+        tmp = []
+        for x in dices:
+            tmp.append(x[random.randrange(0, len(x))])
+        tests.append(sum(tmp))
+    #print(tests)
+    unique = defaultdict(int)
+    for x in tests:
+        unique[x] += 1
+    #print(unique, len(tests))
+    unique = sorted(unique.items(), key=lambda x: x[0])
+    for x in unique:
+        print(f"{x[0]} : {(x[1]/number_of_tests)*100:.2f}%")
