a118c76dc11f2457acf4b6d0319f62f595171ee1
CREED404 <116465479+CREED404@users.noreply.github.com>
2024-03-01 16:11:57 +0100

    Update main.py

--- src/01 - Prime Factors/main.py
+++ src/01 - Prime Factors/main.py
@@ -1,12 +1,43 @@
-def get_prime_factors(number):
-  factors = []
+def allPrimesUpTo(num):
+    factorsList = [1, ]
+    for number in range(2, num):
+        sqr = int(number ** 0.5)+1
+        for numberx in factorsList:
+            if number % numberx == 0 and numberx not in (number, 1):
+                break
+            if numberx > sqr and sqr > 1:
+                factorsList.append(number)
+                break
+        else:
+            #if number not in factorsList:
+            factorsList.append(number)
 
-  divisor = 2
-  while number != 1:
-    if number % divisor == 0:
-      factors.append(divisor)
-      number = number // divisor
-    else:
-      divisor += 1
+def get_prime_factors(num):
+    if num == 1:
+        return [1]
+    elif num <= 0:
+        return 0
+    
+    primeList = allPrimesUpTo(num+1)[1:]
+    pointer = 0
+    result = []
 
-  print(factors)
+    while True:
+        if (num) in primeList:
+            result.append(num)
+            break
+
+        elif (num // primeList[pointer]) == 1:
+            result.append(primeList[pointer])
+            break
+
+        elif (num % primeList[pointer]) == 0:
+            result.append(primeList[pointer])
+            num = num //primeList[pointer]
+            print(result)
+            continue
+
+        else:
+            pointer += 1
+    
+    return result
