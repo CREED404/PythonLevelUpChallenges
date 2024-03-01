e068df6f78814a45b3b1690f1bc0e8e02b6b65a2
CREED404 <116465479+CREED404@users.noreply.github.com>
2024-03-01 22:54:00 +0100

    Update main.py

--- src/09 - Random/main.py
+++ src/09 - Random/main.py
@@ -4,23 +4,20 @@ import random
 def roll_dice(*arg):
     number_of_tests = 1_000_000
     dices = []
+
     for dice in arg:
         tmp = []
         for side in range(1, dice+1):
             tmp.append(side)
         dices.append(tmp)
-    #print(dices)
-    tests = []
+
+    tests = defaultdict(int)
     for _ in range(0, number_of_tests+1):
         tmp = []
         for x in dices:
             tmp.append(x[random.randrange(0, len(x))])
-        tests.append(sum(tmp))
-    #print(tests)
-    unique = defaultdict(int)
-    for x in tests:
-        unique[x] += 1
-    #print(unique, len(tests))
-    unique = sorted(unique.items(), key=lambda x: x[0])
-    for x in unique:
-        print(f"{x[0]} : {(x[1]/number_of_tests)*100:.2f}%")
+        tests[sum(tmp)] += 1
+
+    probabilities = sorted(tests.items(), key=lambda x: x[0])
+    for probability in probabilities:
+        print(f"{probability[0]} : {(probability[1]/number_of_tests)*100:.2f}%")
